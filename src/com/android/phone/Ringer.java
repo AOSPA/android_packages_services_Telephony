5b3cb4d48ee2f7aa879ef4f8967733e97f4b06c2
Jesús David Gulfo Agudelo <jesdga95@gmail.com>
2014-02-18 10:46:31 -0500

    Revert "[3/3]Quiet Hours"
    This reverts commit 032ea5d7171b6d5c8469c47e8d4af89ae273fe38.
    
    Change-Id: I834d1aa509df1e45293aa0f6a748c3b018f475f4

--- src/com/android/phone/Ringer.java
+++ src/com/android/phone/Ringer.java
@@ -35,7 +35,6 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
 
-import com.android.internal.util.paranoid.QuietHoursHelper;
 import com.android.internal.telephony.Phone;
 /**
  * Ringer manager for the Phone app.
@@ -175,8 +174,9 @@ public class Ringer {
                 if (DBG) log("- starting vibrator...");
                 mVibratorThread.start();
             }
+
             int ringerVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_RING);
-            if (ringerVolume == 0 && mRingerVolumeSetting <= 0 || QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_RINGER)) {
+            if (ringerVolume == 0 && mRingerVolumeSetting <= 0) {
                 if (DBG) log("skipping ring because volume is zero");
                 return;
             }
