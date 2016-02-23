33da9a5be56fc62f15281434b1fe3073a11b5baa
Rakesh Pallerla <rakesh@codeaurora.org>
2016-02-23 16:10:46 +0530

    Add perso sub type to IccDeperso showDialog method.
    Add perso sub type to newly introduced showDialog method
    introduced in recent release which invalidated earlier feature.
    
    Change-Id: I7030a39d04ecc813b42761a45158192ff6ef89f5
    CRs-Fixed: 977271

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -261,8 +261,9 @@ public class PhoneGlobals extends ContextWrapper {
                         // Normal case: show the "SIM network unlock" PIN entry screen.
                         // The user won't be able to do anything else until
                         // they enter a valid SIM network PIN.
+                        int subtype = (Integer)((AsyncResult)msg.obj).result;
                         Log.i(LOG_TAG, "show sim depersonal panel");
-                        IccNetworkDepersonalizationPanel.showDialog();
+                        IccNetworkDepersonalizationPanel.showDialog(subtype);
                     }
                     break;
 
