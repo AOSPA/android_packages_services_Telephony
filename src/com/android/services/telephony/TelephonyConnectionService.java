3c4ee375c6b7138fcc2abc3b347df9eeb3657825
Muhammed Siju <quic_c_msiju@quicinc.com>
2023-04-03 18:28:35 +0530

    DSDA: Bypass AOSP DSDA logic for add call
    New DSDA logic added in AOSP has a timeout of 1sec for
    holding the other sub active call before adding a new
    MO call. This is causing issues in add call use cases for
    DSDA. Add a config to disable AOSP logic and bypass the
    AOSP code.
    
    Change-Id: I2176d9f460ae8b88beec2ad13b161b1b9c099fc2
    CRs-Fixed: 3448830

--- src/com/android/services/telephony/TelephonyConnectionService.java
+++ src/com/android/services/telephony/TelephonyConnectionService.java
@@ -1536,7 +1536,9 @@ public class TelephonyConnectionService extends ConnectionService {
                     }
                     return resultConnection;
                 } else {
-                    if (mTelephonyManagerProxy.isConcurrentCallsPossible()) {
+                    if (mTelephonyManagerProxy.isConcurrentCallsPossible() &&
+                        !getApplicationContext().getResources()
+                        .getBoolean(com.android.internal.R.bool.config_disable_aosp_dsda_logic)) {
                         delayDialForOtherSubHold(phone, request.getAccountHandle(), (result) -> {
                             Log.d(this,
                                     "onCreateOutgoingConn - delayDialForOtherSubHold result = "
